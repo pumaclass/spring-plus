2d25bf127a699f73f4caf75ac5d9ec984efefed0
giles <pumaclass@naver.com>
2024-10-04 14:44:50 +0900

    level 1-3 완료

--- src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserAdminController.java
@@ -14,6 +14,7 @@ public class UserAdminController {
 
     private final UserAdminService userAdminService;
 
+
     @PatchMapping("/admin/users/{userId}")
     public void changeUserRole(@PathVariable long userId, @RequestBody UserRoleChangeRequest userRoleChangeRequest) {
         userAdminService.changeUserRole(userId, userRoleChangeRequest);
